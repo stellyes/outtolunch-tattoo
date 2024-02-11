0053960dff0a4c5e19a33736b36b6e0ec69c62fa
Ryan England <me@rengland.org>
2024-02-11 15:33:16 -0800

    Fixed mobile formatting

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,8 +6,7 @@ const Home = () => {
     return (
         <Container className='home d-flex justify-content-center align-items-center'>
             <Col xs={2} className='home-title d-flex flex-column justify-content-center align-items-center'>
-                <h1 className='mb-0'>OUT TO LUNCH</h1>
-                <p className='mt-0 mb-2 p-0 subtitle'>by Luke Zampas</p>
+                <h1 className='mb-2'>OUT TO LUNCH</h1>
                 <div className='bar'></div>
             </Col>
             <Col xs={8} className='home-menu'>
