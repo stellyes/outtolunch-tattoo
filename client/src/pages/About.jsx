1d7f6a7bdeca240f8f91215d957d34953d3bdb16
Ryan England <me@rengland.org>
2024-02-22 15:38:53 -0800

    Added crude about page, and a card on contact page to prevent spam booking through direct messages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,10 +1,67 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import { Link } from 'react-router-dom'
+import { SlArrowLeft } from 'react-icons/sl'
 
 const About = () => {
+
+    const scrambleText = () => {
+        const aboutText = "Luke Zampas runs Out To Lunch tattoo. he's been tattoing for about a decade. anything else you wanna know? book a tattoo.";
+        const fontFamilies = [
+            { "fontFamily": "six-hands-brush, sans-serif" },
+            { "fontFamily": "six-hands-chalk, sans-serif" },
+            { "fontFamily": "six-hands-condensed, sans-serif" },
+            { "fontFamily": "six-hands-marker, sans-serif" },
+            { "fontFamily": "six-hands-rough, sans-serif" },
+            {
+                "fontFamily": "six-hands-black, sans-serif",
+                "fontWeight": 800,
+            }
+        ];
+
+        const textArray = aboutText.split('');
+
+        return (
+            <React.Fragment>
+                {textArray.map((char, index) => {
+                    let i = Math.floor(Math.random() * fontFamilies.length);
+                    return (
+                        <span
+                            key={index}
+                            style={{
+                                fontFamily: fontFamilies[i].fontFamily,
+                                fontWeight: fontFamilies[i].fontWeight,
+                                fontStyle: fontFamilies[i].fontStyle
+                            }}
+                        >
+                            {char}
+                        </span>
+                    );
+                })}
+            </React.Fragment>
+        );
+    };
+
     return (
         <Container>
-            <h1>About</h1>
+            <Container className="about-title my-3">
+                <Row className="d-flex justify-content-center align-items-center">
+                    <Col>
+                        <Link to="/" style={{ color: "#FFF" }}>
+                            <SlArrowLeft />
+                        </Link>
+                    </Col>
+                    <Col className='mb-0 text-end title-text'>
+                        <h1>About</h1>
+                    </Col>
+                </Row>
+            </Container>
+            <Container className='d-flex flex-column align-items-center justify-content-center my-5 p-5' >
+                <h1 className='about-text my-5 py-5 text-center px-3'>{scrambleText()}</h1>
+            </Container>
         </Container>
     )
 };
--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import { Link } from 'react-router-dom'
+import { SlArrowLeft } from 'react-icons/sl'
 
 const Contact = () => {
     return (
         <Container>
-            <h1>Contact</h1>
+            <Container className="contact-title my-3">
+                <Row className="d-flex justify-content-center align-items-center">
+                    <Col>
+                        <Link to="/" style={{ color: "#FFF" }}>
+                            <SlArrowLeft />
+                        </Link>
+                    </Col>
+                    <Col className='mb-0 text-end title-text'>
+                        <h1>Contact</h1>
+                    </Col>
+                </Row>
+            </Container>
+            <Link to='/booking'>
+                <Container className="d-flex flex-row justify-content-center align-items-center">
+                    <Row className='my-3'>
+                        <Col className='booking-check text-center pt-5 pb-4 px-4'>
+                            <h3>Looking to book an appointment?</h3>
+                            <p>Click this box to head over to the booking page and fill out the form!</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </Link>
+            <Container className='contact'>
+
+            </Container>
         </Container>
     )
 };
